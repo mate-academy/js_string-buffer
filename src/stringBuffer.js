837c621d90e2cdad67b42e52c9e48b8e65e92997
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-02-02 18:19:41 +0200

    done

--- src/stringBuffer.js
+++ src/stringBuffer.js
@@ -24,7 +24,13 @@
  * @return {function}
  */
 function makeBuffer() {
-  // write code here
+  let holder = '';
+
+  return (str = '') => {
+    holder += str;
+
+    return holder;
+  };
 }
 
 module.exports = makeBuffer;
