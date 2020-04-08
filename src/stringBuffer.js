3876971018f055409acd1a4ad079fd88ebfda8f8
romarios1987 <romios047@gmail.com>
2020-04-08 06:38:49 +0300

    Solution

--- src/stringBuffer.js
+++ src/stringBuffer.js
@@ -24,7 +24,13 @@
  * @return {function}
  */
 function makeBuffer() {
-  // write code here
+  let buf = '';
+
+  return function buffer(str = '') {
+    buf += str;
+
+    return buf;
+  };
 }
 
 module.exports = makeBuffer;
